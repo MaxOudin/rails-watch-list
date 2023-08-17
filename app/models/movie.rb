8b6c73f0d57396640f62fece350fd70af9e0783a
Maxime Oudin <oudinmaxime@gmail.com>
2023-08-17 11:34:35 +0200

    validations and links

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,7 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, dependent: :destroy
-  has_many :lists, through: :bookmarks
+  has_many :bookmarks
 
   # Validations
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
-  before_destroy :check_for_bookmarks
 end
